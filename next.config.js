d4b6c391068ec1353459a34ccac2cba1b4e885a6
juancarlossilva-MS <jjcarlos853@gmail.com>
2020-12-17 10:08:27 -0300

    'princ'

--- next.config.js
+++ next.config.js
@@ -1,16 +1,29 @@
+
+
 module.exports = {
   webpack: (config) => {
     config.resolve.alias = {
       ...(config.resolve.alias || {}),
       // Transform all direct `react-native` imports to `react-native-web`
       'react-native$': 'react-native-web',
+      'react-native-webview$': 'react-native-web-webview',
     }
+    config.module.rules.push({
+      test: /postMock.html$/,
+      use: {
+        loader: 'file-loader',
+        options: {
+          name: '[name].[ext]',
+        },
+      }
+    })
     config.resolve.extensions = [
       '.web.js',
       '.web.ts',
       '.web.tsx',
       ...config.resolve.extensions,
     ]
+   
     return config
   },
 }
